b62bd0ad8b3ce68c6808aa395713ac379034b03f
Adrian Christian Santos <adrian.crsantos@gmail.com>
2023-07-20 11:51:53 +0800

    Changed: set all items to be visible

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,6 +6,11 @@ return {
     },
     filesystem = {
       hijack_netrw_behavior = "open_current", -- open_default
+      filtered_items = {
+        visible = true, -- Setting this to true will only dimmed out all supposed hidden items.
+        hide_dotfiles = true,
+        hide_gitignored = true,
+      },
     },
   },
 }
