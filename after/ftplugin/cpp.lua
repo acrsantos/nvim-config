728eb76bf0636daba43c16a272f57d65a6bf9b7e
Adrian Christian Santos <adrian.crsantos@gmail.com>
2023-07-22 13:48:15 +0800

    Changed: made cmake command keymaps only available to cpp file types

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -2,3 +2,17 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+local wk = require("which-key")
+wk.register({
+  a = {
+    name = "cmaketools",
+    r = { "<cmd>CMakeRun<cr>", "CMake Run" },
+    s = { "<cmd>CMakeSelectBuildType<cr>", "CMake Select Build Type" },
+    g = { "<cmd>CMakeGenerate<cr>", "CMake Generate" },
+    b = { "<cmd>CMakeBuild<cr>", "CMake Build" },
+    d = { "<cmd>CMakeDebug<cr>", "CMake Debug" },
+    c = { "<cmd>CMakeClean<cr>", "CMake Clean" },
+    i = { "<cmd>CMakeInstall<cr>", "CMake Install" },
+  },
+}, { prefix = "<leader>" })
