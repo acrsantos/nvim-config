b8d7d069a1dd178964260012394d3f5845c3ceaa
Adrian Christian Santos <adrian.crsantos@gmail.com>
2023-07-23 17:19:04 +0800

    Added mason and tree sitter ensured installed

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,18 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "clang-format",
+      "clangd",
+      "codelldb",
+      "eslint-lsp",
+      "js-debug-adapter",
+      "json-lsp",
+      "lua-language-server",
+      "shfmt",
+      "stylua",
+      "tailwindcss-language-server",
+      "typescript-language-server",
+    },
+  },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "c",
+      "cpp",
+      "html",
+      "javascript",
+      "json",
+      "json5",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
+  },
+}
