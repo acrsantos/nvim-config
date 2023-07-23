9ee95278c3e7346d9d96f413d4f57b998867a305
Adrian Christian Santos <adrian.crsantos@gmail.com>
2023-07-23 15:54:54 +0800

    Added sample format

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,16 +3,16 @@
 -- Add any additional keymaps here
 --
 
-local wk = require("which-key")
-wk.register({
-  o = {
-    name = "Obsidian",
-    n = { ":ObsidianNew ", "New" },
-    f = { "<cmd>ObsidianQuickSwitch<cr>", "Quick Switch" },
-    s = { "<cmd>ObsidianSearch<cr>", "Search" },
-    o = { "<cmd>ObsidianOpen<cr>", "Open" },
-    t = { "<cmd>ObsidianToday<cr>", "Today" },
-    y = { "<cmd>ObsidianYesterday<cr>", "Yesterday" },
-    l = { "<cmd>ObsidianLinkNew<cr>", "New Link" },
-  },
-}, { prefix = "<leader>" })
+-- local wk = require("which-key")
+-- wk.register({
+--   o = {
+--     name = "Obsidian",
+--     n = { ":ObsidianNew ", "New" },
+--     f = { "<cmd>ObsidianQuickSwitch<cr>", "Quick Switch" },
+--     s = { "<cmd>ObsidianSearch<cr>", "Search" },
+--     o = { "<cmd>ObsidianOpen<cr>", "Open" },
+--     t = { "<cmd>ObsidianToday<cr>", "Today" },
+--     y = { "<cmd>ObsidianYesterday<cr>", "Yesterday" },
+--     l = { "<cmd>ObsidianLinkNew<cr>", "New Link" },
+--   },
+-- }, { prefix = "<leader>" })
