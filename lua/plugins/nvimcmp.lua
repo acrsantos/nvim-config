37f96d06bb5093fa1ec474feab21b79bd03d7df1
Adrian Christian Santos <adrian.crsantos@gmail.com>
2023-08-07 09:49:58 +0800

    Commented out keymap option for windows

--- lua/plugins/nvimcmp.lua
+++ lua/plugins/nvimcmp.lua
@@ -106,18 +106,26 @@ return {
         }),
       }
       opts.window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+        completion = {
+          border = "rounded",
+          winhighlight = "Normal:Pmenu",
+        },
+        documentation = {
+          border = "rounded",
+          winhighlight = "Normal:Pmenu",
+        },
       }
-      opts.mapping = cmp.mapping.preset.insert({
-        ["<Tab>"] = cmp.mapping.select_next_item(),
-        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-\\>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      })
+      -- opts.mapping = cmp.mapping.preset.insert({
+      --   ["<Tab>"] = cmp.mapping.select_next_item(),
+      --   ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+      --   ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      --   ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      --   ["<C-\\>"] = cmp.mapping.complete(),
+      --   ["<C-e>"] = cmp.mapping.abort(),
+      --   ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      -- })
     end,
   },
 }
