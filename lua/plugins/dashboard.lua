76244b833ffcb944e47a5b07964e9623924e78e0
Adrian Christian Santos <adrian.crsantos@gmail.com>
2023-11-15 13:07:22 +0800

    Added project to dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -58,6 +58,18 @@ return {
       })
     end
 
+    local projects = {
+      action = "Telescope projects",
+      desc = " Projects",
+      icon = " ",
+      key = "p",
+    }
+
+    projects.desc = projects.desc .. string.rep(" ", 43 - #projects.desc)
+    projects.key_format = "  %s"
+
+    table.insert(opts.config.center, 3, projects)
+
     return opts
   end,
 }
