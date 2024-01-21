41d5e2c6876dbf875c277b116f8510abdcdb7d29
Adrian Christian Santos <adrian.crsantos@gmail.com>
2024-01-21 09:31:53 +0800

    change banner

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,7 +14,7 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
   -- title = "hover",
 })
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  virtual_text = false,
+  virtual_text = true,
 })
 
 vim.diagnostic.config({
