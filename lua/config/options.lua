5c4d3ce1ababc1c13c06206ba6a421cb3bcb74c0
Adrian Christian Santos <adrian.crsantos@gmail.com>
2024-02-25 05:35:06 +0800

    remove bufferline

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,32 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = "single",
-})
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  -- Use a sharp border with `FloatBorder` highlights
-  border = "single",
-  -- add the title in hover float window
-  -- title = "hover",
-})
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  virtual_text = {
-    severity = { min = vim.diagnostic.severity.ERROR },
-  },
-})
-
-vim.diagnostic.config({
-  float = {
-    border = "single",
-  },
-})
-
 vim.opt.pumblend = 0
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrw = 1
-
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 10
 
@@ -37,6 +14,27 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.o.guifont = "JetBrainsMono NF:h09"
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  border = "single",
+})
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  -- Use a sharp border with `FloatBorder` highlights
+  border = "single",
+  -- title = "hover",
+})
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+--   virtual_text = {
+--     severity = { min = vim.diagnostic.severity.ERROR },
+--   },
+-- })
+
+vim.diagnostic.config({
+  float = {
+    border = "single",
+  },
+})
 
 vim.api.nvim_create_user_command("RichTextCopy", function(args)
   local saved_html_use_css = vim.g.html_use_css
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,8 +1,5 @@
 return {
-  {
-    -- LSP kind
-    "onsails/lspkind.nvim",
-  },
+  { "onsails/lspkind.nvim" },
   {
     "L3MON4D3/LuaSnip",
     keys = function()
