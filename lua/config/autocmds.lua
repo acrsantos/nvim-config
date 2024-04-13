85a10eef1a41186d33e74547fae9d379ab977132
Adrian Christian Santos <adrian.crsantos@gmail.com>
2024-04-14 07:13:50 +0800

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,7 +3,7 @@
 -- Add any additional autocmds here
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "c", "cpp", "html", "css", "*" },
+  pattern = { "c", "cpp", "*" },
   callback = function()
     vim.b.autoformat = false
   end,
@@ -47,3 +47,33 @@ vim.api.nvim_create_autocmd("CursorMovedI", {
   end,
 })
 
+-- Hide cmd line
+-- vim.opt.cmdheight = 0
+--
+-- vim.api.nvim_create_autocmd('CmdlineEnter', {
+--     group = vim.api.nvim_create_augroup(
+--         'cmdheight_1_on_cmdlineenter',
+--         { clear = true }
+--     ),
+--     desc = 'Don\'t hide the status line when typing a command',
+--     command = ':set cmdheight=1',
+-- })
+--
+-- vim.api.nvim_create_autocmd('CmdlineLeave', {
+--     group = vim.api.nvim_create_augroup(
+--         'cmdheight_0_on_cmdlineleave',
+--         { clear = true }
+--     ),
+--     desc = 'Hide cmdline when not typing a command',
+--     command = ':set cmdheight=0',
+-- })
+--
+-- vim.api.nvim_create_autocmd('BufWritePost', {
+--     group = vim.api.nvim_create_augroup(
+--         'hide_message_after_write',
+--         { clear = true }
+--     ),
+--     desc = 'Get rid of message after writing a file',
+--     pattern = { '*' },
+--     command = 'redrawstatus',
+-- })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,16 +10,59 @@ return {
         desc = "Find Plugin File",
       },
     },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-        -- initial_mode = "normal",
-      },
-    },
+    opts = function()
+      local actions = require("telescope.actions")
+
+      local find_files_no_ignore = function()
+        local action_state = require("telescope.actions.state")
+        local line = action_state.get_current_line()
+        LazyVim.telescope("find_files", { no_ignore = true, default_text = line })()
+      end
+      local find_files_with_hidden = function()
+        local action_state = require("telescope.actions.state")
+        local line = action_state.get_current_line()
+        LazyVim.telescope("find_files", { hidden = true, default_text = line })()
+      end
+
+      return {
+        defaults = {
+          prompt_prefix = " ",
+          selection_caret = " ",
+          -- open files in the first window that is an actual file.
+          -- use the current window if no other window is available.
+          get_selection_window = function()
+            local wins = vim.api.nvim_list_wins()
+            table.insert(wins, 1, vim.api.nvim_get_current_win())
+            for _, win in ipairs(wins) do
+              local buf = vim.api.nvim_win_get_buf(win)
+              if vim.bo[buf].buftype == "" then
+                return win
+              end
+            end
+            return 0
+          end,
+          layout_strategy = "horizontal",
+          layout_config = { prompt_position = "top" },
+          sorting_strategy = "ascending",
+          -- winblend = 0,
+
+          mappings = {
+            i = {
+              ["<a-i>"] = find_files_no_ignore,
+              ["<a-h>"] = find_files_with_hidden,
+              ["<C-Down>"] = actions.cycle_history_next,
+              ["<C-Up>"] = actions.cycle_history_prev,
+              ["<C-f>"] = actions.preview_scrolling_down,
+              ["<C-b>"] = actions.preview_scrolling_up,
+              ["<C-d>"] = actions.delete_buffer + actions.move_to_top,
+            },
+            n = {
+              ["q"] = actions.close,
+            },
+          },
+        },
+      }
+    end,
   },
 
   -- add telescope-fzf-native
